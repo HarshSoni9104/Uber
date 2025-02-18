0b9f7cc4c683f63e7ad56d2a74058b666e297109
HarshSoni9104 <soniharsh9104@gmail.com>
2025-02-19 00:09:54 +0530

    Your commit message

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export const Home = () => {
   return (
     <div>
-        <div className='h-screen flex justify-between flex-col w-full bg-red-400'>
-          <img className='w-10' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-            <div className='bg-white'>
-                <h2>Get started with uber </h2>
-                <button>Continue</button>
+        <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1586805372042-327a923a697a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHRyYWZmaWN8ZW58MHx8MHx8fDA%3D)] h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+          <img className='w-16 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+            <div className='bg-white pb-7 py-5 px-4' >
+                <h2 className='text-3xl font-bold'>Get started with uber </h2>
+               <Link to='/login' className='flex justify-center items-center w-full bg-black text-white py-3 rounded mt-4'>Continue</Link>
             </div>
 
         </div>
--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const UserLogin = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const submitHandler = (e) =>{
+      e.preventDefault()
+      console.log(email,password);
+      setEmail('')
+      setPassword('')
+      
+  }
   return (
-    <div>UserLogin</div>
+    <>
+      <div className='bg-black w-full '>
+        <img className='w-30 ml-8' src="https://cdn.mos.cms.futurecdn.net/M4hbiWhoo8n3bQQ2hKg5s3.jpg" alt="" />
+      </div>
+      <div className='p-7 h-screen flex flex-col justify-between'>
+        <div>
+          <form onSubmit={(e) => {
+              submitHandler(e)
+          }}>
+          <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+          <input
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+            className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+            type="email"
+            placeholder='email@example.com'
+          />
+          <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+            required type="password"
+            placeholder='password'
+          />
+          <button
+            className='bg-[#111] text-[#fff] font-semibold mb-2 rounded px-4 py-2  w-full text-lg placeholder:text-base'
+          >Login</button>
+        </form>
+          <p className='text-center'>New here? <Link to='/signup' className='text-blue-600'>Create new Account</Link></p>
+        </div>
+        <div>
+          <button
+            className='bg-[#10b461] text-[#fff] font-semibold mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base'>Sign in as Captain</button>
+        </div>
+      </div>
+    </>
   )
 }
